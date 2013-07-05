27de12f45303aab67a57071269cd7a22b4e95e98
testa <testa.a.tapos@gmail.com>
2013-07-05 13:33:24 +0100

    Add json out

--- headers/sys/out.php
+++ headers/sys/out.php
@@ -24,4 +24,53 @@ function osonout($outdata){
     }
 }
 
+function osonoutjsonstart(){
+    global $osonsysvar;
+    if (isset($osonsysvar['outpacknum'])) {
+        if($osonsysvar['outpacknum']==0){
+            osonout('[{');
+        }else{
+            osonout(',{');	
+        }
+    } else {
+    	  $osonsysvar['outpacknum']=0;
+    	  osonout('[{');
+    }
+    $osonsysvar['outpacknum']++;
+    $osonsysvar['outtagpacknum'] = 0 ;
+}
+	
+function osonoutjsontag($outtagname, $outtagvalue){
+    global $osonsysvar;
+    if ($osonsysvar['outtagpacknum']>0){
+        osonout(',');
+    }
+    osonout('"');
+    osonout($outtagname);
+    osonout('":"');
+    osonout($outtagvalue);
+    osonout('"');
+    $osonsysvar['outtagpacknum']++;
+}	
+	
+function osonoutjsontagend() {
+    global $osonsysvar;    	  
+    osonout('}');
+    $osonsysvar['outtagpacknum'] = 0;
+}	
+	
+function osonoutjsonend() {
+    global $osonsysvar;
+    if($osonsysvar['outpacknum']>0){
+        osonout(']');
+    }	
+}	
+	
+	
+function osonoutdatanum(){
+    global $osonsysvar;	
+    osonouttag("appid", $osoninputvars['osonappid']);    
+    osonouttag("datanum", $osonsysvar['outpacknum']);
+}
+
 ?>
